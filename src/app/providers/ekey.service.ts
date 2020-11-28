c2f43153cbff700ae0845262a34a76bf87ee22b8
Guidipolito <guilhermepereiradipolito@hotmail.com>
2020-11-27 21:55:04 -0300

    adicionado forma de inserir solicitação no banco de dados, tentei ao maximo fazer por Post mas falhei,  então fiz por Get

--- src/app/providers/ekey.service.ts
+++ src/app/providers/ekey.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient} from '@angular/common/http';
 import  { Observable } from 'rxjs';
 
 @Injectable({
@@ -19,4 +19,12 @@ export class EkeyService {
   			return res;
   		});
   	}
+
+    insereSolicita(email, discord, telefone, msg, equipe, nick): Observable<object>{
+      //Eu realmente tentei fazer isso por Post mas n consegui por nada
+      return this.http.get('http://localhost/processo/apiinseresolicita.php?email='+email+"&telefone="+telefone+"&discord="+discord+"&nick="+nick+"&msg="+msg+"&nomequipe="+equipe).pipe((res: any) => {
+        return res;
+      });
+    }
+
 }
--- src/app/solicita/solicita.page.ts
+++ src/app/solicita/solicita.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { EkeyService } from '../providers/ekey.service';
 import { ActivatedRoute } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';;
 
 @Component({
   selector: 'app-solicita',
@@ -9,10 +11,91 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class SolicitaPage implements OnInit {
 
-  constructor(public route: ActivatedRoute, public chave: EkeyService) { }
+  constructor(public route: ActivatedRoute, public chave: EkeyService, public alerta: AlertController, public router: Router) { }
 nick;
+detail;
   ngOnInit() {
   	this.nick = this.route.snapshot.paramMap.get('nick')
   }
+  email:string; discord:string; telefone:string; msg:string; equipe:string; //declarando as variaveis com dados dos inputs, acho que finalmente entendi isso
+  solicitaAgoraVai(){
+  	if (this.email!=null && this.email!="" && this.equipe !=null && this.equipe!="") {
 
+
+  		this.chave.insereSolicita(this.email, this.discord, this.telefone, this.msg, this.equipe, this.nick).subscribe((dados: [{}]) =>{
+  			console.log(dados);
+  			this.detail = dados;
+  			this.queAlertaGrande(this.detail.resposta);
+  		});
+
+  	}else{
+  		this.queAlertaGrande('erro');
+  	}
+  }
+
+
+  async queAlertaGrande(tipo) {
+  	const alertaErro = await this.alerta.create({
+  		header: 'Tem algo errado',
+  		message: 'O campo obrigatório Email ou nome da equipe esta em falta',
+  		buttons: [
+  		{
+  			text: 'Voltar',
+  			role: 'cancel',
+  			cssClass: 'secondary',
+  			handler: (blah) => {
+  				console.log('Voltar');
+  			}
+  		
+  		}
+  		]
+  	});
+
+  	const sucesso = await this.alerta.create({
+  		header: 'Tudo certo',
+  		message: 'A solicitação foi enviada',
+  		buttons: [
+  		{
+  			text: 'Voltar',
+  			role: 'cancel',
+  			cssClass: 'secondary',
+  			handler: (blah) => {
+
+  				console.log('Voltar');
+  				this.router.navigate(['/faz-chave/'+this.nick]);
+  			}
+  		
+  		}
+  		]
+  	});
+
+	const falha = await this.alerta.create({
+  		header: 'Lamento...',
+  		message: 'Por algum motivo não foi possivel enviar sua solicitação',
+  		buttons: [
+  		{
+  			text: 'Voltar',
+  			role: 'cancel',
+  			cssClass: 'secondary',
+  			handler: (blah) => {
+
+  				console.log('Voltar');
+  				
+  			}
+  		
+  		}
+  		]
+  	});
+
+	//ve qual alerta tem q se exibir e mostra
+  	switch (tipo) {
+  		case "erro": await alertaErro.present(); break;
+  		case "certo": await sucesso.present(); break;
+  		default: await falha.present(); break;
+ 
+  	}
+  	
+
+  }	
+  
 }
